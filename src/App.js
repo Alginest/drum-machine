f759672a9ba55e4456a27b16eae8eb57f099e105
alginest <ado.durak22@gmail.com>
2023-02-08 20:19:41 +0100

    react project completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import { arr } from "./DrumNotes";
+console.log(arr);
 function App() {
+  const [drums, setDrums] = useState(arr);
+  const [activeKey, setActiveKey] = useState("");
+  const [wrongKey, setWrongKey] = useState("");
+  function playSound(selector) {
+    const audio = document.getElementById(selector);
+    if (audio !== null) {
+      audio.play();
+      setWrongKey("");
+    } else {
+      setWrongKey("Wrong Key");
+    }
+    setActiveKey(selector);
+  }
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => {
+      playSound(e.key.toUpperCase());
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div id="drum-machine">
+        <div className="drumContainer">
+          <div className="drumPads">
+            {drums.map((drum) => {
+              const { src, text, keyCode } = drum;
+              return (
+                <div
+                  onClick={() => {
+                    playSound(text);
+                  }}
+                  className="drum-pad"
+                  key={keyCode}
+                  id={src}
+                >
+                  <audio controls className="clip" id={text} src={src}></audio>
+                  {text}
+                </div>
+              );
+            })}
+          </div>
+          <div id="display">
+            <div className="activeKey">
+              {activeKey} {wrongKey}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
